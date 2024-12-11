417152154ba263d4272e1d9dbdd937ffc933bc50
minjun <qkralswns1998@gmail.com>
2024-12-11 20:35:32 +0900

    style: 스타일을 예제와 비슷하게 조금 변경.

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -15,24 +15,24 @@ const Champions = async () => {
 
   return (
     <div className="grid grid-cols-4 gap-4 p-24">
-        {data.map(([key, value], i) => {
-          return (
-            <Link
-              className="border rounded p-4 hover:shadow-lg"
-              href={"/"}
-              key={i}
-            >
-              <Image
-                src={`https://ddragon.leagueoflegends.com/cdn/14.24.1/img/champion/${key}.png`}
-                width={50}
-                height={50}
-                alt="챔피언 이미지"
-              ></Image>
-              <p>{value.name}</p>
-            </Link>
-          );
-        })}
-
+      {data.map(([key, value], i) => {
+        return (
+          <Link
+            className="border rounded p-4 hover:shadow-lg"
+            href={"/"}
+            key={i}
+          >
+            <Image
+              src={`https://ddragon.leagueoflegends.com/cdn/14.24.1/img/champion/${key}.png`}
+              width={50}
+              height={50}
+              alt="챔피언 이미지"
+            ></Image>
+            <h2 className="mt-2 text-xl font-semibold">{value.name}</h2>
+            <p className="text-gray-500">{value.title}</p>
+          </Link>
+        );
+      })}
     </div>
   );
 };
