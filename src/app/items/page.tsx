ef172e7f878c487a8092051e7b7cd54165507e66
minjun <qkralswns1998@gmail.com>
2024-12-13 10:56:01 +0900

    feat: 아이템 디테일 페이지 추가.

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -1,14 +1,19 @@
 import { cleanText } from "@/utils/cleanText";
 import { fetchItemData } from "@/utils/serverApi";
 import Image from "next/image";
+import Link from "next/link";
 
 const Items = async () => {
-  const data = await fetchItemData();
+  const { ItemData } = await fetchItemData();
   return (
     <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 p-24">
-      {data.map(([key, value]) => {
+      {ItemData.map(([key, value]) => {
         return (
-          <div className="border rounded p-4 hover:shadow-lg" key={key}>
+          <Link
+            href={`/items/${key}`}
+            className="border rounded p-4 hover:shadow-lg"
+            key={key}
+          >
             <Image
               src={`https://ddragon.leagueoflegends.com/cdn/14.24.1/img/item/${key}.png`}
               width={100}
@@ -16,8 +21,7 @@ const Items = async () => {
               alt={`${value.name}에 대한 이미지입니다.`}
             ></Image>
             <h2 className="mt-2 text-xl font-semibold">{value.name}</h2>
-            <p className="text-gray-500">{cleanText(value.description)}</p>
-          </div>
+          </Link>
         );
       })}
     </div>
